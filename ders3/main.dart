4003db440b1b59be4c73fe105911109fb6847d05
YunusDemirr <mustafayavass52@gmail.com>
2025-10-20 16:38:57 +0300

    ders2

---
+++ ders3/main.dart
@@ -0,0 +1,13 @@
+void main() {
+  String adSoyad = "Mustafa Yavaş";
+  int yas = 29;
+  bool medeniDurum = true;
+  double maas = 10000.5;
+  var adres = "Gebze";
+  dynamic telefonNumarasi = 54565465;
+  adres = "Çayırova";
+  telefonNumarasi = 10.4;
+  print("Merhaba $adSoyad Yaşınız: $yas");
+  print("Merhaba " + adSoyad +"Yaşınız: "+ yas.toString());
+  print(maas);
+}
