28079533359cb392ad8fdda6f4651613f03e52f0
YunusDemirr <mustafayavass52@gmail.com>
2025-10-27 17:17:13 +0300

    ders5

--- ders5/main.dart
+++ ders5/main.dart
@@ -2,13 +2,19 @@
 //1.IF-ELSE (EĞER - DEĞİLSE)
 //2.SWITCH-CASE
 void main() {
-  int sayi = 7;
+  int sayi = -5;
   if (sayi % 2 == 0) //KOŞUL
   {
     print("Bu sayı çifttir.");
-  } 
-  else 
-  {
+  } else {
     print("Bu sayı tektir.");
+  }
+
+  if (sayi < 0) {
+    print("$sayi sayısı negatiftir.");
+  } else if (sayi > 0) {
+    print("$sayi sayısı pozitiftir.");
+  } else {
+    print("$sayi sayısı nötrdür.");
   }
 }
